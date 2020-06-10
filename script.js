3d6052c069238238aff929e006de98f5606ed95a
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-06-10 16:04:20 -0500

    Stubbed Javascript functions, working with TA to ensure game logic is sound

---
+++ script.js
@@ -0,0 +1,27 @@
+//------> Declaring DOM Refs and Consts
+
+
+function image () {
+    let img = document.createElement('img');
+    let allBoxes = document.getElementsByClassName('img')
+    let randomBox = allBoxes[Math.floor(Math.random() * 9)]
+}
+
+
+
+
+//should assign random
+//on click, the card should turn over and have an img
+// should stay open and allow you to 
+// init function should clear game, assign pics randomly, 
+
+// document.getElementById('nameInput').addEventListener('submit', e => {
+//     e.preventDefault();
+//     console.log('hi!')
+
+// })
+
+// //this will go in the init function
+// const imgAssign = () => {
+// }
+
