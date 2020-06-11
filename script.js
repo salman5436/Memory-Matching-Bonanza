99d2d945129f52c2daf994a1e2124b46596479e8
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-06-11 17:59:21 -0500

    Attempting to store the turned images through Javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//---------------------> Declaring DOM Refs and Consts <--------------------------
+//-----------------------------> Declaring DOM Refs and Consts <----------------
 //array story links
 
 //array of rapper images that will be pushed within a function
@@ -8,25 +8,37 @@ let links =
 'https://picsum.photos/278/181',
 'https://www.placecage.com/278/181']
 
+const player = {
+    points: 0,
+    pick: null,
+    choices: []
+}
+
 
 //this var represents an array of the elements in my HTML with the class card, meaning the img divs on my game board
 let allCards = document.getElementsByClassName('card')
 
-
-const playerChoices = {
-    points: 0,
-    choice: null,
-    cardsClicked = []
-}
-
-
-// -----------------------> Game Logic Function <-----------------------
-
+let card1 = document.getElementById('00')
+let card2 = document.getElementById('01')
+let card3 = document.getElementById('02')
+let card4 = document.getElementById('03')
+let card5 = document.getElementById('04')
+let card6 = document.getElementById('05')
+let card7 = document.getElementById('06')
+let card8 = document.getElementById('07')
 
 
+//--------------------> Game Logic Functions <------------------------
 
-// // This function is a callBack Function that is called when adding the event listener in the loop for the AllCards array
+
+// This function is a callBack Function that is called when adding the event listener in the loop for the AllCards array
 // function flipCards (e) {
+//      //changed image versus default image
+//      // if div id at img is the same as default image, do THIS
+//      if () {
+
+//     }
+    
 //     console.log(e);
 //     //card is the creation of a new img element where links[url] will go
 //     let card = document.createElement('img');
@@ -34,47 +46,63 @@ const playerChoices = {
 //     // Using this var, 
 //     let randomImg = links[Math.floor(Math.random() * 5)]
 //     e.target.src = randomImg;
+//     hasFlipped = true;
 // }
+
 
 // //This loop iterates through the div elements on my game board and adds an event listener with the function flipCards (above)
-// function randomizeCards (){
-//     for (var i = 0; i < allCards.length; i++) {
-//         allCards[i].addEventListener('click', flipCards)
-//     }
+// for (var i = 0; i < allCards.length; i++) {
+//     allCards[i].addEventListener('click', flipCards)
+    
+   
 // }
 
-function flipCards (e) {
-    console.log(e);
-    //card is the creation of a new img element where links[url] will go
-    let card = document.createElement('img');
-        
-    var j = links[Math.floor(Math.random() * (i + 1))];
-        var temp = array[i];
-    e.target.src = array[j];
-}
-
-function randomizeCards() {
-    for (var i = allCards.length - 1; i > 0; i--) {
-        allCards[i].addEventListener('click', flipCards)
-        
-    }
-}
-
+//How can I ensure
 // Check for a match based on url
-// function checkMatch () {
-//    //So, the function should match one card's flipped img with another card's flipped img, if not it will turn the card back upside down
-//     if (e.target)
-// }
+function swapImg () {
+    
+}
 
+
+function storeImg () {
+    card1.addEventListener('click', function () {
+        card1.src = 'https://placekitten.com/278/181'
+    })
+    card2.addEventListener('click', function () {
+        card2.src = 'https://picsum.photos/id/237/278/181'
+    })
+    card3.addEventListener('click', function () {
+        card3.src = 'https://picsum.photos/278/181'
+    })
+    card4.addEventListener('click', function () {
+        card4.src = 'https://placekitten.com/278/181'
+    })
+    card5.addEventListener('click', function () {
+        card5.src = 'https://picsum.photos/id/237/278/181'
+    })
+    card6.addEventListener('click', function () {
+        card6.src = 'https://placekitten.com/278/181'
+    })
+    card7.addEventListener('click', function () {
+        card7.src = 'https://www.placecage.com/278/181'
+    })
+    card8.addEventListener('click', function () {
+        card8.src = 'https://placekitten.com/278/181'
+    }) 
+}
+
+storeImg();
 
 //init start function will clear the board and do the flipCard function and start the game
-function init () {
-    randomizeCards();
-    playerChoices.points = 0;
-    playerChoices.choice = null;
-    playerChoices.cardsClicked = [];
-}
-
 //game logic: win condition must be established
 // I have to disable double click and ensure that pictures stay open if they match, otherwise restore original src ** This is probably the most challenging roadblock for today
 
+//How can I map my ids to the images that are stored/replaced
+
+//HOw to prevent user to flipping more than 2 cards;
+// 2 scenarios:
+// 1 - match
+// 2 - non match
+
+// 1: Base on a match system that forces cards to flip back if not matched
+// 2: Counter system through a loop that resets to 0 after 2 turns
